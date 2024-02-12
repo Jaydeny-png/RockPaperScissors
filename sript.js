d42f6165b018ba76f33eddff8f0e18629b6863c9
Jaydeny-png <jyau1020@gmail.com>
2024-02-12 10:41:19 -0500

    Day 1

---
+++ sript.js
@@ -0,0 +1,21 @@
+function originalYear() {
+    document.getElementById('diffImage').src = 'images/me.jpg'
+    document.getElementById('change-paragraph').innerText = 'See that lil baby ghost? Thats me'
+    document.getElementById('changeDate').innerText = 'October 20th, 2007'
+}
+
+function year1964() {
+    document.getElementById('diffImage').src = 'images/herbert.webp'
+    document.getElementById('change-paragraph').innerText = "Herbert Hoover, the 31st President of the United States, died on October 20, 1964, at the age of 90 in New York City. The funeral for Hoover was held on October 23, 1964, and he was buried in West Branch, Iowa, next to his Presidential Library and Museum."
+    document.getElementById('changeDate').innerText = 'October 20th, 1964'
+}
+function year1971() {
+    document.getElementById('diffImage').src = 'images/Him.webp'
+    document.getElementById('change-paragraph').innerText = 'Kenneth Choi born October 20, 1971 is an Asian American actor.'
+    document.getElementById('changeDate').innerText = 'October 20th, 1971'
+}
+function year19712() {
+    document.getElementById('diffImage').src = 'images/dogg.jpg'
+    document.getElementById('change-paragraph').innerText = 'Snoop Dogg, born on October 20, 1971, is an American rapper, singer, songwriter, producer, media personality, and actor. He is known for his relaxed style of rapping and love for cannabis culture.'
+    document.getElementById('changeDate').innerText = 'October 20th, 1971'
+}
