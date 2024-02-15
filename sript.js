c437223430dce14168d15ff4b50b25d57a191e40
Jaydeny-png <jyau1020@gmail.com>
2024-02-15 09:30:54 -0500

    Jesus help

--- sript.js
+++ sript.js
@@ -1,20 +1,83 @@
-function originalYear() {
-    document.getElementById('diffImage').src = 'images/me.jpg'
-    document.getElementById('change-paragraph').innerText = 'See that lil baby ghost? Thats me'
-    document.getElementById('changeDate').innerText = 'October 20th, 2007'
-}
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper' || userInput === 'bomb') {
+    return userInput;
+  } else {
+    console.log('Error, please type: rock, paper or scissors.');
+    }
+  }
+  
+  
+  const getComputerChoice = () => {
+   const randomNumber = Math.floor (Math.random()*3);
+   switch (randomNumber){
+    case 0:
+    return 'rock';
+    case 1:
+    return 'paper';
+    case 2:
+    return 'scissors';
+   }
+  };
+  
+  
+  const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+      return 'The game is a tie!';
+    }
+    if (userChoice ===  'rock') {
+      if (computerChoice ===  'paper') {
+        return "Sorry, computer won!";
+      } else {
+        return "Congratulations, you won!";
+      }
+    }
+  
+  
+    if (userChoice ===  'paper') {
+      if (computerChoice ===  'scissors') {
+        return "Sorry, computer won!";
+      } else {
+        return "Congratulations, you won!";
+      }
+    }
+  
+  
+    if (userChoice ===  'scissors') {
+      if (computerChoice ===  'rock') {
+        return "Sorry, computer won!";
+      } else {
+        return "Congratulations, you won!";
+      }
+    }
+  
+  }
+  
+  
+  const playGame = () => {
+    const userChoice = getUserChoice('paper');
+    const computerChoice = getComputerChoice();
+    console.log('You threw ' + userChoice);
+    console.log('The computer threw ' + computerChoice);
+  
+  
+    console.log(determineWinner(userChoice, computerChoice));
+  };
 
-function year1964() {
+  
+  playGame()
+  
+function Rock() {
     document.getElementById('diffImage').src = 'images/herbert.webp'
     document.getElementById('change-paragraph').innerText = "Herbert Hoover, the 31st President of the United States, died on October 20, 1964, at the age of 90 in New York City. The funeral for Hoover was held on October 23, 1964, and he was buried in West Branch, Iowa, next to his Presidential Library and Museum."
     document.getElementById('changeDate').innerText = 'October 20th, 1964'
 }
-function year1971() {
+function Peper() {
     document.getElementById('diffImage').src = 'images/Him.webp'
     document.getElementById('change-paragraph').innerText = 'Kenneth Choi born October 20, 1971 is an Asian American actor.'
     document.getElementById('changeDate').innerText = 'October 20th, 1971'
 }
-function year19712() {
+function Scissors() {
     document.getElementById('diffImage').src = 'images/dogg.jpg'
     document.getElementById('change-paragraph').innerText = 'Snoop Dogg, born on October 20, 1971, is an American rapper, singer, songwriter, producer, media personality, and actor. He is known for his relaxed style of rapping and love for cannabis culture.'
     document.getElementById('changeDate').innerText = 'October 20th, 1971'
